28137d32c94ccde4b7de53591c5098ce26437989
FSuaverdez <sfrannz@gmail.com>
2022-01-10 17:58:11 +0800

    removed unused state and imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import { authActions } from './app/slices/authSlice'
