d89e4dc3f275ab12d97f135cca11420a3b053f7c
FrannzSAS <sfrannz@gmail.com>
2022-04-14 15:21:29 +0800

    remove console.log

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -15,7 +15,6 @@ export const addCategory = async (req, res) => {
 export const getCategories = async (req, res) => {
   try {
     const categories = await Category.find({});
-    console.log(categories);
 
     res.status(200).json(categories);
   } catch (error) {
