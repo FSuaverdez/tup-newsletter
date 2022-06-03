e0b7705f1fb86d414799752ba89328cdff2ba743
Angelo Baclaan <angelo_baclaan26@yahoo.com>
2022-06-03 22:28:09 +0800

    Loading Animation in Posting

--- frontend/src/components/PostLoading/PostCompleteModal.jsx
+++ frontend/src/components/PostLoading/PostCompleteModal.jsx
@@ -4,12 +4,14 @@ const PostCompleteModal = ({classes,handleCloseCompleted}) => {
 
     return (
         <div
-        className={`lg:w-656  sm:w-340 shadow-xl bg-white p-5 rounded ${classes}`}
+        className={`flex flex-row lg:w-656  sm:w-340 shadow-xl bg-white p-5 rounded ${classes}`}
         onClick={e => e.stopPropagation()}
         >
-            <p className='text-xl font-bold'>Your post is now submitted. It is now pending for approval.</p>
+            <div className='ml-3 mt-1.5'>
+                <p className='text-xl font-bold'>Your post is now submitted. Pending for admin approval.</p>
+            </div>
 
-            <div className='flex gap-2 justify-end mt-20'>
+            <div className='ml-5 mt-0.5'>
                 <Button type='success' onClick={handleCloseCompleted}>
                         Close
                 </Button>
--- frontend/src/components/PostLoading/PostLoadingModal.jsx
+++ frontend/src/components/PostLoading/PostLoadingModal.jsx
@@ -1,13 +1,23 @@
+import React, { useState } from "react";
+import { MetroSpinner } from "react-spinners-kit";
 
 const PostLoadingModal = (classes) => {
+    const [loading, setLoading] = useState(true)
 
     return (
-        <div
-        className={`lg:w-656  sm:w-340 shadow-xl bg-white p-5 rounded ${classes}`}
-        onClick={e => e.stopPropagation()}
-        >
-            Loading...
+    
+        <div className={`flex flex-row justify-center lg:w-656  sm:w-340 shadow-xl bg-white p-5 pl-0 rounded `}>
+
+            <div className='justify-center mt-1 mb-1'>
+                <MetroSpinner  size={40} color="#FF2400" />
+            </div>
+
+            <div className='justify-center ml-5 mt-2.5'>
+                <p className='text-xl font-bold'>Loading, Kindly wait for a moment.</p>
+            </div>       
+
         </div>
-    )
+    );
 }
+
 export default PostLoadingModal
