fa87a56ea5cafd39059eef10ceb1bb2bbd2ab0e9
FSuaverdez <sfrannz@gmail.com>
2022-06-11 19:50:36 +0800

    add creator

--- backend/models/SubCategory.js
+++ backend/models/SubCategory.js
@@ -16,6 +16,10 @@ const subCategorySchema = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Category',
     },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
     userPermissions: {
       type: [
         {
