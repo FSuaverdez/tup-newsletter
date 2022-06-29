519b990d6d8bdc8282ae615fb297372024722efb
FSuaverdez <sfrannz@gmail.com>
2022-06-29 17:07:18 +0800

    update featured

--- frontend/src/components/Featured/Featured.jsx
+++ frontend/src/components/Featured/Featured.jsx
@@ -1,48 +1,48 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
-import img1 from '../../assets/background.jpg'
+import img1 from '../../assets/background.jpg';
 import { useGetAllFeaturedPostsQuery } from '../../app/services/postApi';
 import JoditEditor from 'jodit-react';
-const Featured = (props) => {
-    const {config} = props;
-    const {
-        data,
-        isLoading,
-        isFetching,
-      } = useGetAllFeaturedPostsQuery();
-    const getImage = (string) => {
-        let arr;
-        if (string.match(/<img s([\w\W]+?)>/g)) {
-            arr = string.match(/<img s([\w\W]+?)>/g);
-        } else {
-            arr = '';
-        }
-       
-      return arr[0] || `<img src = ${img1} />`
+import parse from 'html-react-parser';
+const Featured = props => {
+  const { config } = props;
+  const { data, isLoading, isFetching } = useGetAllFeaturedPostsQuery();
+  const getImage = string => {
+    let arr;
+    if (string.match(/<img s([\w\W]+?)>/g)) {
+      arr = string.match(/<img s([\w\W]+?)>/g);
+    } else {
+      arr = '';
     }
-    const breakPoints =[
-        {width: 1, itemsToShow: 1},
-        {width: 550, itemsToShow: 2},
-        {width: 768, itemsToShow: 3},
-        {width: 1200, itemsToShow: 4}
-    ];
 
-    return (
-        <div>
-            <Carousel breakPoints = {breakPoints} showThumbs={false} swipeable={true} autoplay={true}>
-              {data?.map((c)=>{
-                return(
-                    <div key={c._id}>
-                          <JoditEditor
-                            value={getImage(c.content)}
-                            config={config}
-                          />
-                        <p className="legend text-xl font-bold">{c.title}</p>
-                    </div>
-                )
-              })}
-            </Carousel>
-        </div>
-    )
-}
-export default Featured
+    return arr[0] || `<img src = ${img1} />`;
+  };
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
+  return (
+    <div>
+      <Carousel
+        breakPoints={breakPoints}
+        showThumbs={false}
+        swipeable={true}
+        autoplay={true}
+      >
+        {data?.map(c => {
+          return (
+            <div key={c._id} className='featured'>
+              {/* <JoditEditor value={getImage(c.content)} config={config} /> */}
+              <div className='featured-img'>{parse(getImage(c.content))} </div>
+              <p className='legend text-xl font-bold'>{c.title}</p>
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
+export default Featured;
